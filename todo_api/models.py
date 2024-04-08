c3028410f68f9f29f136b6507de871cfc1bedfd0
OwenOrcan <owenorcan@gmail.com>
2024-04-07 23:51:21 -0400

    Fixed the created at DATA to only show the day

--- todo_api/models.py
+++ todo_api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Todo(models.Model):
     task_name = models.CharField(max_length=255)
     priority = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.task_name
