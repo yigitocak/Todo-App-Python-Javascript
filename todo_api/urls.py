f005544a5590569b8ff1993da9fb0a1614130b7f
OwenOrcan <owenorcan@gmail.com>
2024-04-08 23:20:55 -0400

    added the finish url to serve the view

--- todo_api/urls.py
+++ todo_api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import task_delete, task_list, task_create
+from .views import task_delete, task_list, task_create, task_finish
 
 urlpatterns = [
     path('tasks/', task_list, name='task-list'),
     path('tasks/create/', task_create, name='task-create'),
     path('tasks/delete/<int:id>/', task_delete, name='task-delete'),
+    path('tasks/finish/<int:id>/', task_finish, name='task-finish')
 ]
