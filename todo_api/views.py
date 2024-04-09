ad94bef4c363877d3351406b846dc5fa75df2ad9
OwenOrcan <owenorcan@gmail.com>
2024-04-08 22:10:55 -0400

    Changed from CBVs to FBVs

--- todo_api/views.py
+++ todo_api/views.py
@@ -1,16 +1,33 @@
-from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from .models import Todo
 from .serializers import TodoSerializer
+from django.http import Http404
+from rest_framework import status
 
-class TodoListView(generics.ListAPIView):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
+@api_view(['GET'])
+def todo_list(request):
+    if request.method == 'GET':
+        todos = Todo.objects.all()
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data)
 
-class TodoCreateView(generics.CreateAPIView):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
+@api_view(['POST'])
+def todo_create(request):
+    if request.method == 'POST':
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
 
-class TodoDeleteView(generics.DestroyAPIView):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
-    lookup_field = 'id'
+@api_view(['DELETE'])
+def todo_delete(request, id):
+    try:
+        todo = Todo.objects.get(id=id)
+    except Todo.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'DELETE':
+        todo.delete()
+        return Response({"message:" "Task Deleted"} ,status=status.HTTP_204_NO_CONTENT)
