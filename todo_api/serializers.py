fabdfa1a7912b4a92f7bb1e4e37b976ca7c49b09
OwenOrcan <owenorcan@gmail.com>
2024-04-08 22:26:47 -0400

    change serializer name to TaskSerializer and make it compatible with the new model

--- todo_api/serializers.py
+++ todo_api/serializers.py
@@ -1,10 +1,10 @@
 # todos/serializers.py
 from rest_framework import serializers
-from .models import Todo
+from .models import Task
 
 
-class TodoSerializer(serializers.ModelSerializer):
+class TaskSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Todo
+        model = Task
         fields = ['id', 'task_name', 'priority',
                   'created_at']
