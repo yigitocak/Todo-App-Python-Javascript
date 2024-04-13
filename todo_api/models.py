68fa7a0992ef41976c9930a42042f620a2ebf6a4
OwenOrcan <owenorcan@gmail.com>
2024-04-12 23:46:10 -0400

    Delete the created_at field

--- todo_api/models.py
+++ todo_api/models.py
@@ -5,7 +5,6 @@ class Task(models.Model):
     task_name = models.CharField(max_length=255)
     priority = models.IntegerField()
     status = models.CharField(max_length=255, default='open')
-    created_at = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.task_name
